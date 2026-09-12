83cc4e8f251ddb45ba9b56e46993d20260a9db71
alyusr-real-estate <alyusrreal@gmail.com>
2026-09-12 13:25:29 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,507 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reception-vue-1",
+      "name": "Reception vue 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9552211105178312,
+          "pitch": 0.2736114469240185,
+          "rotation": 4.71238898038469,
+          "target": "2-reception-vue-3"
+        },
+        {
+          "yaw": 0.050034559317051475,
+          "pitch": -0.25883839844834,
+          "rotation": 3.9269908169872414,
+          "target": "7-reception-vue-4"
+        },
+        {
+          "yaw": 0.1891789050253987,
+          "pitch": 0.04048141280071249,
+          "rotation": 2.356194490192345,
+          "target": "8-salle-de-physiothrapie-vue-1"
+        },
+        {
+          "yaw": -0.7154059006859814,
+          "pitch": 0.062176823707034146,
+          "rotation": 4.71238898038469,
+          "target": "3-salle-de-consulation-vue-1"
+        },
+        {
+          "yaw": -0.9552207527931706,
+          "pitch": -0.03400514715825054,
+          "rotation": 4.71238898038469,
+          "target": "5-salle-de-mecanothrapie-vue-1"
+        },
+        {
+          "yaw": -0.5974844444682397,
+          "pitch": -0.2685457746054709,
+          "rotation": 2.356194490192345,
+          "target": "1-reception-vue-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-reception-vue-2",
+      "name": "Reception vue 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.228641880131633,
+          "pitch": 0.32385511952828416,
+          "rotation": 3.9269908169872414,
+          "target": "2-reception-vue-3"
+        },
+        {
+          "yaw": 0.6826210449460888,
+          "pitch": 0.2698143689716854,
+          "rotation": 3.9269908169872414,
+          "target": "0-reception-vue-1"
+        },
+        {
+          "yaw": 0.9002288037472947,
+          "pitch": 0.19165427526725232,
+          "rotation": 2.356194490192345,
+          "target": "5-salle-de-mecanothrapie-vue-1"
+        },
+        {
+          "yaw": 0.987990175448406,
+          "pitch": -0.14488584458531228,
+          "rotation": 1.5707963267948966,
+          "target": "3-salle-de-consulation-vue-1"
+        },
+        {
+          "yaw": -0.908111221990179,
+          "pitch": -0.11189195276629249,
+          "rotation": 2.356194490192345,
+          "target": "7-reception-vue-4"
+        },
+        {
+          "yaw": -0.6591315452993012,
+          "pitch": 0.19764677595019542,
+          "rotation": 0.7853981633974483,
+          "target": "8-salle-de-physiothrapie-vue-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-reception-vue-3",
+      "name": "Reception vue 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8532697437071182,
+          "pitch": 0.41584680231743576,
+          "rotation": 0.7853981633974483,
+          "target": "0-reception-vue-1"
+        },
+        {
+          "yaw": -0.8780118793718543,
+          "pitch": -0.06575210271556742,
+          "rotation": 4.71238898038469,
+          "target": "5-salle-de-mecanothrapie-vue-1"
+        },
+        {
+          "yaw": -0.6231953997776802,
+          "pitch": -0.062110205273262054,
+          "rotation": 4.71238898038469,
+          "target": "3-salle-de-consulation-vue-1"
+        },
+        {
+          "yaw": 0.43074639601253395,
+          "pitch": -0.3514680405625157,
+          "rotation": 3.9269908169872414,
+          "target": "7-reception-vue-4"
+        },
+        {
+          "yaw": -0.20334724472774646,
+          "pitch": -0.3177435916758693,
+          "rotation": 2.356194490192345,
+          "target": "1-reception-vue-2"
+        },
+        {
+          "yaw": 0.5450366306701895,
+          "pitch": -0.005251282541259172,
+          "rotation": 1.5707963267948966,
+          "target": "8-salle-de-physiothrapie-vue-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salle-de-consulation-vue-1",
+      "name": "Salle de Consulation vue 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6309207924599463,
+          "pitch": -0.2977171057443222,
+          "rotation": 2.356194490192345,
+          "target": "4-salle-de-consulation-vue-2"
+        },
+        {
+          "yaw": -0.8570735738352635,
+          "pitch": 0.21823256603292052,
+          "rotation": 3.141592653589793,
+          "target": "1-reception-vue-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-salle-de-consulation-vue-2",
+      "name": "Salle de Consulation vue 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6591294593709414,
+          "pitch": -0.30458226834970326,
+          "rotation": 3.141592653589793,
+          "target": "3-salle-de-consulation-vue-1"
+        },
+        {
+          "yaw": 0.6270710182959185,
+          "pitch": 0.28757271074195145,
+          "rotation": 0,
+          "target": "1-reception-vue-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salle-de-mecanothrapie-vue-1",
+      "name": "Salle de Mecanothérapie vue 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.428311166948383,
+          "pitch": 0.11207070469878389,
+          "rotation": 0,
+          "target": "0-reception-vue-1"
+        },
+        {
+          "yaw": 0.9401744793106968,
+          "pitch": -0.16014853223630077,
+          "rotation": 3.9269908169872414,
+          "target": "6-salle-de-mecanothrapie-vue-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-salle-de-mecanothrapie-vue-2",
+      "name": "Salle de Mecanothérapie vue 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9047514940524906,
+          "pitch": -0.14102305511162072,
+          "rotation": 1.5707963267948966,
+          "target": "5-salle-de-mecanothrapie-vue-1"
+        },
+        {
+          "yaw": 0.2728588572239836,
+          "pitch": 0.17959203655916411,
+          "rotation": 0,
+          "target": "0-reception-vue-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-reception-vue-4",
+      "name": "Reception vue 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5454102251456128,
+          "pitch": -0.23720462401023745,
+          "rotation": 3.9269908169872414,
+          "target": "1-reception-vue-2"
+        },
+        {
+          "yaw": -0.011782118652748252,
+          "pitch": 0.16060506916746675,
+          "rotation": 0,
+          "target": "3-salle-de-consulation-vue-1"
+        },
+        {
+          "yaw": -0.2755885315987463,
+          "pitch": 0.042492061427170214,
+          "rotation": 0,
+          "target": "5-salle-de-mecanothrapie-vue-1"
+        },
+        {
+          "yaw": -0.8792123515976016,
+          "pitch": 0.06939743432010204,
+          "rotation": 2.356194490192345,
+          "target": "2-reception-vue-3"
+        },
+        {
+          "yaw": -0.5012256626884355,
+          "pitch": 0.22107638688915543,
+          "rotation": 2.356194490192345,
+          "target": "0-reception-vue-1"
+        },
+        {
+          "yaw": -0.7574249551135601,
+          "pitch": 0.37517003261757154,
+          "rotation": 3.141592653589793,
+          "target": "8-salle-de-physiothrapie-vue-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-salle-de-physiothrapie-vue-1",
+      "name": "Salle de Physiothérapie vue 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008836765786696787,
+          "pitch": -0.076435097744465,
+          "rotation": 3.141592653589793,
+          "target": "9-salle-de-physiothrapie-vue-2"
+        },
+        {
+          "yaw": -0.7384292340787457,
+          "pitch": 0.21656756191285886,
+          "rotation": 3.9269908169872414,
+          "target": "7-reception-vue-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-salle-de-physiothrapie-vue-2",
+      "name": "Salle de Physiothérapie vue 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19201862883651089,
+          "pitch": -0.384649335680578,
+          "rotation": 2.356194490192345,
+          "target": "8-salle-de-physiothrapie-vue-1"
+        },
+        {
+          "yaw": -0.04121597417779199,
+          "pitch": 0.22285928072661143,
+          "rotation": 5.497787143782138,
+          "target": "7-reception-vue-4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ALYUSR CONSTRUCTION - PhisioPulse",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
